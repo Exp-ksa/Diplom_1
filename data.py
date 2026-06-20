ce478e2393b594f166dead9bf4436f4cfe985049
Sergei Kuznetsov <lamaku@mail.ru>
2026-06-20 20:04:20 +0800

    unit test

---
+++ data.py
@@ -0,0 +1,46 @@
+import praktikum.ingredient_types as ingredient_types
+
+black_bun = ("black bun", 100)
+white_bun = ("white bun", 200)
+red_bun = ("red bun", 300)
+
+hot_sauce = (ingredient_types.INGREDIENT_TYPE_SAUCE, "hot sauce", 100)
+chili_sauce = (ingredient_types.INGREDIENT_TYPE_SAUCE, "chili sauce", 300)
+sour_cream = (ingredient_types.INGREDIENT_TYPE_SAUCE, "sour cream", 200)
+cutlet = (ingredient_types.INGREDIENT_TYPE_FILLING, "cutlet", 100)
+dinosaur = (ingredient_types.INGREDIENT_TYPE_FILLING, "dinosaur", 200)
+sausage =(ingredient_types.INGREDIENT_TYPE_FILLING, "sausage", 300)
+
+ingredient_data_2 = [
+            hot_sauce,
+            sausage
+        ]
+
+ingredient_data_3 = [
+            hot_sauce,
+            chili_sauce,
+            sausage
+        ]
+
+ingredient_data_4 = [
+            chili_sauce,
+            sour_cream,
+            dinosaur,
+            sausage
+        ]
+
+ingredient_data_5 = [
+            hot_sauce,
+            chili_sauce,
+            sour_cream,
+            dinosaur,
+            sausage
+        ]
+ingredient_data_6 = [
+            hot_sauce,
+            chili_sauce,
+            sour_cream,
+            cutlet,
+            dinosaur,
+            sausage
+        ]
